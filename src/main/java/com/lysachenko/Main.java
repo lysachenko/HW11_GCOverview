ddccf371affa2de7cdd47036e253dbf1b62fa4d7
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-07-21 23:04:36 +0300

    init commit

---
+++ src/main/java/com/lysachenko/Main.java
@@ -0,0 +1,12 @@
+package com.lysachenko;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int size = 100;
+        while (true) {
+            String[] array = new String[size];
+            size *= size * 2;
+        }
+    }
+}
