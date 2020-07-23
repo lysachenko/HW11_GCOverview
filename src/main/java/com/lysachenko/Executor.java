70776fc1bc373b501395f2c73b87546f302c646d
Roman Lysachenko <lysachenko.roman.1099@gmail.com>
2020-07-23 20:06:07 +0300

    create Executor class and and change size

---
+++ src/main/java/com/lysachenko/Executor.java
@@ -0,0 +1,11 @@
+package com.lysachenko;
+
+public class Executor {
+    public void run() {
+        int size = 1000;
+        while (true) {
+            String[] array = new String[size];
+            size *= 2;
+        }
+    }
+}
--- src/main/java/com/lysachenko/Main.java
+++ src/main/java/com/lysachenko/Main.java
@@ -3,10 +3,6 @@ package com.lysachenko;
 public class Main {
 
     public static void main(String[] args) {
-        int size = 100;
-        while (true) {
-            String[] array = new String[size];
-            size *= size * 2;
-        }
+        new Executor().run();
     }
 }
